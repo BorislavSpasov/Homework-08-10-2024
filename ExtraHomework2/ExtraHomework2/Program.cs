f10b0a50e4cd6a27bca45e0d1d13cb1c91100ceb
BorislavSpasov <borislav.e.spasov@gmail.com>
2024-11-11 18:21:47 +0200

    Add files via upload

---
+++ ExtraHomework2/ExtraHomework2/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("Please input Jimmy's max jump height: ");
+int jumpHeight = int.Parse(Console.ReadLine());
+
+Console.Write("Please input the number of tower floors: ");
+int tower = int.Parse(Console.ReadLine());
+
+if (jumpHeight <= 0)
+{
+    Console.WriteLine("Jimmy couldn't jump at all so he never went to the tower.");
+    return;
+}
+else if (tower == 0)
+{
+    Console.WriteLine("There was never a tower and Jimmy just sat at home and enjoyed a cold beer.");
+    return;
+}
+else if (tower < 0)
+{
+    Console.WriteLine("Turns out the tower was a hole all along and Jimmy just went home dissapointed.");
+    return;
+}
+
+int distanceResult = 0;
+
+int[] array = new int[tower];
+
+string jimmyResult = null;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Please input the lenght of tower floor number " + (i + 1) + ": ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] <= jumpHeight)
+    {
+        distanceResult += array[i];
+    }
+    else
+    {
+        jimmyResult = "Sadly Jimmy couldn't reach the top of the tower. :(";
+        break;
+    }
+    jimmyResult = "Jimmy managed to reach the top of the tower! 🎉";
+}
+
+
+Console.WriteLine("Jimmy has ascended a total distance of: " + distanceResult + ". " + jimmyResult);
---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("How long would you like the number squence to be: ");
+int repetitions = int.Parse(Console.ReadLine());
+
+if (repetitions <= 0)
+{
+    Console.WriteLine("Please restart the program and input a positive number!");
+}
+else
+{
+    int outputNum = 1;
+    int counter = 0;
+    int addingNum = 2;
+
+    while (counter < repetitions)
+    {
+        Console.Write(outputNum + ", ");
+
+        outputNum *= 2;
+        outputNum += addingNum;
+        addingNum -= 1;
+
+        counter++;
+    }
+}
